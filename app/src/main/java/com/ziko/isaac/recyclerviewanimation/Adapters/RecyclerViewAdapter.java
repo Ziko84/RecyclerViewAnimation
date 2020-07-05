aaee319cb5901f0c8efb877451c204d76c35d9ca
Ziko <zikobooking@gmail.com>
2020-07-05 17:45:28 +0300

    Fade Animation added to the user image

--- app/src/main/java/com/ziko/isaac/recyclerviewanimation/Adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/ziko/isaac/recyclerviewanimation/Adapters/RecyclerViewAdapter.java
@@ -1,47 +1,79 @@
 package com.ziko.isaac.recyclerviewanimation.Adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ziko.isaac.recyclerviewanimation.Model.News;
+import com.ziko.isaac.recyclerviewanimation.R;
 
 import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
     private Context context;
     private List<News> mData;
+    private RelativeLayout container;
+
+    public RecyclerViewAdapter(Context context, List<News> mData) {
+        this.context = context;
+        this.mData = mData;
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_news, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
+        //Animation for Image
+        holder.img_user.setAnimation(AnimationUtils.loadAnimation(context, R.anim.fade_transition_animation));
+
+        //Animation for the whole card
+        holder.container.setAnimation(AnimationUtils.loadAnimation(context, R.anim.fade_transition_animation));
+
+
+
+
+        holder.tv_title.setText(mData.get(position).getTitle());
+        holder.tv_description.setText(mData.get(position).getDescription());
+        holder.tv_date.setText(mData.get(position).getDate());
+
+        holder.img_user.setImageResource(mData.get(position).getImg());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
 
         private TextView tv_title;
         private TextView tv_description;
         private TextView tv_date;
         private ImageView img_user;
+        private RelativeLayout container;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            container = itemView.findViewById(R.id.container);
+            tv_title = itemView.findViewById(R.id.tv_title);
+            tv_description = itemView.findViewById(R.id.tv_description);
+            tv_date = itemView.findViewById(R.id.tv_date);
+            img_user = itemView.findViewById(R.id.img_user);
         }
 
     }
--- app/src/main/java/com/ziko/isaac/recyclerviewanimation/MainActivity.java
+++ app/src/main/java/com/ziko/isaac/recyclerviewanimation/MainActivity.java
@@ -1,14 +1,116 @@
 package com.ziko.isaac.recyclerviewanimation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.ziko.isaac.recyclerviewanimation.Adapters.RecyclerViewAdapter;
+import com.ziko.isaac.recyclerviewanimation.Model.News;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView newsRecyclerView;
+    private List<News> mData;
+    RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //make this activity a full screen
+        makeActivityFullScreen();
+
         setContentView(R.layout.activity_main);
+
+        //hide the action bar
+        getSupportActionBar().hide();
+
+        //init RecyclerView and List
+        newsRecyclerView = findViewById(R.id.news_recycler_view);
+        mData = new ArrayList<>();
+
+        //filling the list
+        fillTheList();
+
+        //Set the Adapter
+        adapterSetup();
+
+
+    }
+
+    private void makeActivityFullScreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    private void adapterSetup() {
+        adapter = new RecyclerViewAdapter(this, mData);
+        newsRecyclerView.setAdapter(adapter);
+        newsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void fillTheList() {
+        mData.add(new News("Java SE 8", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout", "4th July 2014", R.drawable.circle2));
+        mData.add(new News("Xcode Device", "It is a long established fact that a reader will be distracted at its layout", "4th July 2024", R.drawable.circle3));
+        mData.add(new News("Android Studio", "It is a long established fact that a reader will be distracted by the readable content of a page when looking  layout", "4th June 2004", R.drawable.circle1));
+        mData.add(new News("After Effect", "It is a long established fact that a reader will be distracted by the readable content of", "4th July 1984", R.drawable.circle4));
+        mData.add(new News("Adobe Photoshop", "It is a long established fact that a reader will be distracted by the reada its layout", "4th July 1974", R.drawable.circle5));
+        mData.add(new News("Adobe Illustrator", "It is a long established fact that a reader will ben looking at its layout", "4th July 1567", R.drawable.circle6));
+        mData.add(new News("Adobe Premiere", "It is a long established fact that a reader will be distracted by the readable at its layout", "4th July 1098", R.drawable.circle6));
+        mData.add(new News("Java SE 8", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout", "4th July 2014", R.drawable.circle2));
+        mData.add(new News("Xcode Device", "It is a long established fact that a reader will be distracted at its layout", "4th July 2024", R.drawable.circle3));
+        mData.add(new News("Android Studio", "It is a long established fact that a reader will be distracted by the readable content of a page when looking  layout", "4th June 2004", R.drawable.circle1));
+        mData.add(new News("After Effect", "It is a long established fact that a reader will be distracted by the readable content of", "4th July 1984", R.drawable.circle4));
+        mData.add(new News("Adobe Photoshop", "It is a long established fact that a reader will be distracted by the reada its layout", "4th July 1974", R.drawable.circle5));
+        mData.add(new News("Adobe Illustrator", "It is a long established fact that a reader will ben looking at its layout", "4th July 1567", R.drawable.circle6));
+        mData.add(new News("Adobe Premiere", "It is a long established fact that a reader will be distracted by the readable at its layout", "4th July 1098", R.drawable.circle6));
+        mData.add(new News("Java SE 8", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout", "4th July 2014", R.drawable.circle2));
+        mData.add(new News("Xcode Device", "It is a long established fact that a reader will be distracted at its layout", "4th July 2024", R.drawable.circle3));
+        mData.add(new News("Android Studio", "It is a long established fact that a reader will be distracted by the readable content of a page when looking  layout", "4th June 2004", R.drawable.circle1));
+        mData.add(new News("After Effect", "It is a long established fact that a reader will be distracted by the readable content of", "4th July 1984", R.drawable.circle4));
+        mData.add(new News("Adobe Photoshop", "It is a long established fact that a reader will be distracted by the reada its layout", "4th July 1974", R.drawable.circle5));
+        mData.add(new News("Adobe Illustrator", "It is a long established fact that a reader will ben looking at its layout", "4th July 1567", R.drawable.circle6));
+        mData.add(new News("Adobe Premiere", "It is a long established fact that a reader will be distracted by the readable at its layout", "4th July 1098", R.drawable.circle6));
+        mData.add(new News("Java SE 8", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout", "4th July 2014", R.drawable.circle2));
+        mData.add(new News("Xcode Device", "It is a long established fact that a reader will be distracted at its layout", "4th July 2024", R.drawable.circle3));
+        mData.add(new News("Android Studio", "It is a long established fact that a reader will be distracted by the readable content of a page when looking  layout", "4th June 2004", R.drawable.circle1));
+        mData.add(new News("After Effect", "It is a long established fact that a reader will be distracted by the readable content of", "4th July 1984", R.drawable.circle4));
+        mData.add(new News("Adobe Photoshop", "It is a long established fact that a reader will be distracted by the reada its layout", "4th July 1974", R.drawable.circle5));
+        mData.add(new News("Adobe Illustrator", "It is a long established fact that a reader will ben looking at its layout", "4th July 1567", R.drawable.circle6));
+        mData.add(new News("Adobe Premiere", "It is a long established fact that a reader will be distracted by the readable at its layout", "4th July 1098", R.drawable.circle6));mData.add(new News("Java SE 8", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout", "4th July 2014", R.drawable.circle2));
+        mData.add(new News("Xcode Device", "It is a long established fact that a reader will be distracted at its layout", "4th July 2024", R.drawable.circle3));
+        mData.add(new News("Android Studio", "It is a long established fact that a reader will be distracted by the readable content of a page when looking  layout", "4th June 2004", R.drawable.circle1));
+        mData.add(new News("After Effect", "It is a long established fact that a reader will be distracted by the readable content of", "4th July 1984", R.drawable.circle4));
+        mData.add(new News("Adobe Photoshop", "It is a long established fact that a reader will be distracted by the reada its layout", "4th July 1974", R.drawable.circle5));
+        mData.add(new News("Adobe Illustrator", "It is a long established fact that a reader will ben looking at its layout", "4th July 1567", R.drawable.circle6));
+        mData.add(new News("Adobe Premiere", "It is a long established fact that a reader will be distracted by the readable at its layout", "4th July 1098", R.drawable.circle6));
+        mData.add(new News("Java SE 8", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout", "4th July 2014", R.drawable.circle2));
+        mData.add(new News("Xcode Device", "It is a long established fact that a reader will be distracted at its layout", "4th July 2024", R.drawable.circle3));
+        mData.add(new News("Android Studio", "It is a long established fact that a reader will be distracted by the readable content of a page when looking  layout", "4th June 2004", R.drawable.circle1));
+        mData.add(new News("After Effect", "It is a long established fact that a reader will be distracted by the readable content of", "4th July 1984", R.drawable.circle4));
+        mData.add(new News("Adobe Photoshop", "It is a long established fact that a reader will be distracted by the reada its layout", "4th July 1974", R.drawable.circle5));
+        mData.add(new News("Adobe Illustrator", "It is a long established fact that a reader will ben looking at its layout", "4th July 1567", R.drawable.circle6));
+        mData.add(new News("Adobe Premiere", "It is a long established fact that a reader will be distracted by the readable at its layout", "4th July 1098", R.drawable.circle6));
+        mData.add(new News("Java SE 8", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout", "4th July 2014", R.drawable.circle2));
+        mData.add(new News("Xcode Device", "It is a long established fact that a reader will be distracted at its layout", "4th July 2024", R.drawable.circle3));
+        mData.add(new News("Android Studio", "It is a long established fact that a reader will be distracted by the readable content of a page when looking  layout", "4th June 2004", R.drawable.circle1));
+        mData.add(new News("After Effect", "It is a long established fact that a reader will be distracted by the readable content of", "4th July 1984", R.drawable.circle4));
+        mData.add(new News("Adobe Photoshop", "It is a long established fact that a reader will be distracted by the reada its layout", "4th July 1974", R.drawable.circle5));
+        mData.add(new News("Adobe Illustrator", "It is a long established fact that a reader will ben looking at its layout", "4th July 1567", R.drawable.circle6));
+        mData.add(new News("Adobe Premiere", "It is a long established fact that a reader will be distracted by the readable at its layout", "4th July 1098", R.drawable.circle6));
+        mData.add(new News("Java SE 8", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout", "4th July 2014", R.drawable.circle2));
+        mData.add(new News("Xcode Device", "It is a long established fact that a reader will be distracted at its layout", "4th July 2024", R.drawable.circle3));
+        mData.add(new News("Android Studio", "It is a long established fact that a reader will be distracted by the readable content of a page when looking  layout", "4th June 2004", R.drawable.circle1));
+        mData.add(new News("After Effect", "It is a long established fact that a reader will be distracted by the readable content of", "4th July 1984", R.drawable.circle4));
+        mData.add(new News("Adobe Photoshop", "It is a long established fact that a reader will be distracted by the reada its layout", "4th July 1974", R.drawable.circle5));
+        mData.add(new News("Adobe Illustrator", "It is a long established fact that a reader will ben looking at its layout", "4th July 1567", R.drawable.circle6));
+        mData.add(new News("Adobe Premiere", "It is a long established fact that a reader will be distracted by the readable at its layout", "4th July 1098", R.drawable.circle6));
     }
 }
